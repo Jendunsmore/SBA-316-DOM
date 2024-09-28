08f05e8ffbc02c5e8ae1634687fe41f8d1e37151
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-09-27 23:24:27 -0400

    worked on script.js file. still in progress

---
+++ script.js
@@ -0,0 +1,90 @@
+/*
+REQUIREMENTS:
+--------------------
+1. Cache at least 1 element using | selectElementById
+
+
+2. Cache at least 1 element using | querySelector or querySelectorAll
+
+
+3. Use the parent-child-sibling relationship to navigate between elements
+at least once (firstChild, LastChild, parentNode, nextElementSibling, etc)
+
+
+4. Iterate over a collection of elements to accomplish some task
+
+
+5. Create at least 1 element using | createElement
+
+
+6. Use appendChild and/or prepend to add new elements to the DOM.
+
+
+7. Use the DocumentFragment interface or HTML templated with the cloneNode
+method to create templated content.
+
+
+8. Modify the HTML or text content of at least one element in response to user
+interaction using innerHTML, innerText, or textContent.
+
+
+9. Modify the style and/or CSS classes of an element in response to user interactions
+using the style or classList properties.
+
+
+10. Modify at least one attribute of an element in response to user interaction.
+
+
+11. Register at least two different event listeners and create the associated event
+handler functions.
+
+
+12. Use at least two Browser Object Model (BOM) properties or methods.
+
+
+13. Include at least one form and/or input with HTML attribute validation.
+
+
+14. Include at least one form and/or input with DOM event-based validation. (This can
+be the same form or input as the one above, but should include event-based
+validation in addition to the HTML attribute validation.)
+
+
+15. Ensure that the program runs without errors (comment out things that do not work,
+and explain your blockers - you can still receive partial credit).
+
+
+16. Commit frequently to the git repository.
+17. Include a README file that contains a description of your application.
+18. Level of effort displayed in creativity, presentation, and user experience.
+*/
+//-----------------------------------------------------
+
+
+//------ Cache elements from HTML --------------------------
+const mainHeader = document.getElementById('main-header');
+const nameForm = document.querySelector('#name-form');
+const usernameInput = document.querySelector('#username');
+const startGameSection = document.getElementById('start-game');
+const gameContainer = document.getElementById('game-container');
+const questionContainer = document.getElementById('question-container');
+const nextButton = document.getElementById('next-btn');
+const resultContainer = document.getElementById('result-container');
+const finalScoreText = document.getElementById('final-score');
+const restartButton = document.getElementById('restart-btn');
+
+//------- Questions Array ----------------------------------------
+const questions = [
+    { question:             }
+    { question:             }
+    { question:             }
+    { question:             }
+    { question:             }
+    { question:             }
+]
+
+let questionIndex = 0;
+let score = '';
+
+//------- Form Submission------------------------------------------
+
